b0594976083ee78eac93f22b971b45903aa568f8
Ahmet <ahmet-yildirim_5858@outlook.com>
2021-04-17 22:19:22 +0300

    Done

--- Assets/Scripts/GameOverController.cs
+++ Assets/Scripts/GameOverController.cs
@@ -7,6 +7,9 @@ public class GameOverController : MonoBehaviour
     [SerializeField] private Text totalScoreText;
     [SerializeField] private Text gameOverLabel;
     [SerializeField] private AudioClip gameOverAudioClip;
+    [SerializeField] private Text highScoreText1;
+    [SerializeField] private Text highScoreText2;
+    [SerializeField] private Text highScoreText3;
     
     public void OnTriggerEnter2D(Collider2D other)
     {
@@ -18,7 +21,23 @@ public class GameOverController : MonoBehaviour
             AudioSource.PlayClipAtPoint(gameOverAudioClip, Camera.main.transform.position);
             Time.timeScale = 0;
             gameOverPanel.SetActive(true);
-            gameOverLabel.text = $"GAME OVER ASDASDASD {PlayerPrefs.GetString("name")}";
+            ScoreController.Instance.SaveHighScore(ScoreController.Instance.GetScore());
+            var scores = ScoreController.Instance.GetHighScoresList();
+            
+            highScoreText1.text = $"SCORE: {scores[0]}";
+            highScoreText2.text = $"SCORE: {scores[1]}";
+            highScoreText3.text = $"SCORE: {scores[2]}";
+            /*for (int j = 0; j < scores.Count; j++)
+            {
+                int height = 3;
+                var text = Instantiate(highScoreText);
+                text.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -height * j);
+                //highScoreText2.text = $"SCORE: {score}";
+                text.GetComponent<Text>().text = $"SCORE: {scores[j]}";
+                Debug.Log("HİGHSCORES: "+ scores[j]);
+            }*/
+            Debug.Log(PlayerPrefs.GetString("name"));
+            gameOverLabel.text = $"GAME OVER {PlayerPrefs.GetString("name")}";
             totalScoreText.text = $"Total Score: {ScoreController.Instance.GetScore().ToString()}";
         }
     }
--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -10,6 +10,10 @@ public class ScoreController : MonoBehaviour
     public static ScoreController Instance;
     [SerializeField] private Text textScore;
     private int score;
+    
+    
+    private List<int> highScores = new List<int>();
+
 
     private void Awake()
     {
@@ -23,26 +27,83 @@ public class ScoreController : MonoBehaviour
         {
             Instance = this;
         }
+        
+        //PlayerPrefs.DeleteAll();
+        //SaveHighScore(3);
     }
 
     public void IncreaseScore(int amount = 1)
     {
         score += amount;
         textScore.text = score.ToString();
-        if (SceneManager.GetActiveScene().name == "Level1" && score == 3)
+        if (SceneManager.GetActiveScene().name == "Level1" && score == 30)
         {
             PlayerPrefs.SetInt("score", score);
             SceneController.Instance.NextLevel(2);
         }
-        else if (SceneManager.GetActiveScene().name == "Level2" && score == 6)
+        else if (SceneManager.GetActiveScene().name == "Level2" && score == 30+31)
         {
             PlayerPrefs.SetInt("score", score);
             SceneController.Instance.NextLevel(3);
         }
+        else if (SceneManager.GetActiveScene().name == "Level3" && score == 61+29)
+        {
+            Debug.Log("WON");
+            SceneManager.LoadScene("Start");
+        }
     }
-    
+
     public int GetScore()
     {
         return score;
+    }
+
+    public List<int> GetHighScoresList()
+    {
+        if (PlayerPrefs.HasKey("highscores"))
+        {
+            string getHighScoresJSON = PlayerPrefs.GetString("highscores");
+            var fromJsonHighScores = JsonUtility.FromJson<HighScores>(getHighScoresJSON);
+            return fromJsonHighScores.HighScoreList;
+        }
+
+        return new List<int>();
+    }
+    
+    public void SaveHighScore(int highScore)
+    {
+        if (PlayerPrefs.HasKey("highscores"))
+        {
+            string getHighScoresJSON = PlayerPrefs.GetString("highscores");
+            var fromJsonHighScores = JsonUtility.FromJson<HighScores>(getHighScoresJSON);
+            
+            if (fromJsonHighScores.HighScoreList.Count >= 3)
+            {
+                fromJsonHighScores.HighScoreList.RemoveAt(0);
+            }
+            
+            highScores = fromJsonHighScores.HighScoreList;
+            highScores.Add(highScore);
+            var highScoresClass = new HighScores {HighScoreList = highScores};
+            string json = JsonUtility.ToJson(highScoresClass);
+            PlayerPrefs.SetString("highscores", json);
+        }
+        else
+        {
+            highScores.Add(highScore);
+            var highScoresClass = new HighScores
+            {
+                HighScoreList = highScores
+            };
+            
+            string toJsonHighScores = JsonUtility.ToJson(highScoresClass);
+            PlayerPrefs.SetString("highscores", toJsonHighScores);
+        }
+    }
+    
+    [Serializable]
+    public class HighScores
+    {
+        public List<int> HighScoreList;
     }
 }
